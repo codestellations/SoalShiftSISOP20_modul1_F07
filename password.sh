70043db5ff3ffcb2f0947e9f6561eb4f440cb991
liizzasulistio <58472359+liizzasulistio@users.noreply.github.com>
2020-02-21 16:55:12 +0700

    Soal 2

---
+++ password.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Please input your file name: "
+read pass_file
+
+genpass() #a function to generate random password
+{
+  pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 28)
+}
+
+i=0
+
+
+while [ $i -eq 0 ]
+do
+	if [[ $pass =~ [A-Z] ]]
+	then
+		if [[ $pass=~[a-z] ]]
+		then
+			if [[ $pass=~[0-9] ]]
+			then i=1
+			else genpass
+			fi
+		else genpass
+		fi
+	else genpass
+fi
+done
+
+
+echo $pass >> $pass_file.txt
