6bd9f41661c9ba0d0ca7a3ca2c5942424db0a7f0
codestellations <erlindaargyt@gmail.com>
2020-02-21 17:37:43 +0700

    Revisi soal 3

---
+++ soal3/crontab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#soal 3.b : crontab tiap 8 jam dimulai dari 6.05 tiap hari kecuali sabtu
+
+#5 6,*/8 * * 1-5,7 bash /home/el/Documents/GitHub/SoalShiftSISOP20_modul1_F07/soal3.sh
--- soal3/soal3.sh
+++ soal3/soal3.sh
@@ -3,10 +3,11 @@
 #Soal 3.a : script untuk mendownload 28 gambar dari alamat dengan command wget dan file "pdkt_kusuma_no"
 #           log messages wget ke file "wget.log"
 
-a=10
+a=1
 no=1
 
-until [ $a -eq 10 ]
+satu(){
+until [ $a -eq 28 ]
 do
  name="pdkt_kusuma_$no"
  #echo $name
@@ -14,46 +15,26 @@ do
   wget -O /home/el/Downloads/pdkt_kusuma_$no "https://loremflickr.com/320/240/cat" 2>&1 | tee -a /home/el/Downloads/wget.log
   no=$((no + 1))
 done
+}
 
-
-#Soal 3.b : penjadwalan tiap 8 jam mulai jam 6.05 tiap hari kecuali sabtu
-#           script memindahkan gambar identik gambar berbeda disimpan di folder /kenangan
-
+satu
 
 #Soal 3.c : script identifikasi gambar identik dari keseluruhan gambar yang didownload
 #           duplikat diberi nama "duplicate_no" dan dipindah ke folder ./duplicate dengan nama "kenangan_no"
 #           setelah tidak ada gambar di current directory, backup log dengan .log.bak
 
-grep -e 'Location' -e 'Saving' /home/el/Downloads/wget.log > /home/el/Downloads/location.log
-paste -d " " - - < /home/el/Downloads/location.log | sort -u -k2 > /home/el/Downloads/kenangan.log
-awk '$2 in a {print a[$2]} {a[$2]=$0}' /home/el/Downloads/kenangan.log > /home/el/Downloads/duplicate.log
-#dupes=${grep -o ''.*'' duplicate.log | sed 's/'//g'}
-#filename=$(awk '/home/ {print $6}' /home/el/Downloads/duplicate.log)
-
-#grep -io "/home.*\'" /home/el/Downloads/duplicate.log | cut -d "'" -f2 
-#echo "${satu::-1}"
-#| cut -d"'" -f2
-#grep -o ''.*'' /home/el/Downloads/duplicate.log | tr -d "'"
-#awk -F\' ' '{print $6}' /home/el/Downloads/duplicate.log
-#grep -o "\'.*\'" /home/el/Downloads/duplicate.log
-#huft=$(sed -n "s/^.*'\(.*\)'.*$/\1/p" <<< $filename)
-#echo "$huft"
-#grep "to: '" /home/el/Downloads/duplicate.log | cut -d "'" -f4
-#sed 's/^[^"]*"\([^"]*\)".*/\1/'
-#grep -o ''.*'' /home/el/Downloads/duplicate.log | sed 's/"//g'
-#grep -v "/home/el/Downloads/.*'" /home/el/Downloads/duplicate.log
-#sed -E 's/.*\('(.*)')\.*/\1/'
-#grep -oP "'.*'" /home/el/Downloads/duplicate.log
-
 cd /home/el/Downloads
 
-finddup(){
- find . ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD > /home/el/Downloads/test.log
-}
-
-#cd /home/el/Downloads
 num=1
 ber=1
+
+finddup(){
+ dir="/home/el/Downloads/"
+ find . ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD | awk -F" " 'NR%2==0 {print $2}' > /home/el/Downloads/duplicate.log
+# sed -i 's/"."/$dir/g' /home/el/Downloads/duplicate.log
+}
+
+finddup
 
 ken(){
  for file in pdkt_kusuma_*
